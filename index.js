6b8cc4f5dc1c8be0f1b51e7360a6f32dee04f78a
webbuilder123 <31453129+webbuilder123@users.noreply.github.com>
2018-01-29 02:42:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
     const start = parseInt(query.s) || 0
     const timeout = parseInt(query.t) || 2000
     const size = parseInt(query.sz) || 1000
-    request('https://www.us-proxy.org/', (e, r, b) => {
+    request('https://www.proxy.org/', (e, r, b) => {
         if (e) throw e
         if (!e && r.statusCode === 200) {
             let arr = []
