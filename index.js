0f4f42f6264a446aebb4e43d25adfc181b3cf16c
webbuilder123 <31453129+webbuilder123@users.noreply.github.com>
2018-01-29 02:29:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,53 +3,54 @@ const cheerio = require('cheerio')
 const app = require('express')()
 const url = require('url')
 app.set("view engine", "ejs")
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     const query = url.parse(req.url, true).query
     console.log(query)
-    const len = parseInt(query.l) || 10
+    const len = parseInt(query.l) || 30
     const start = parseInt(query.s) || 0
     const timeout = parseInt(query.t) || 2000
-    const size = parseInt(query.sz) || 50000
-    request('https://www.site.org/', (e, r, b)=>{
-        if(e) throw e
-        if(!e && r.statusCode===200){
+    request('https://www.us-proxy.org/', (e, r, b) => {
+        if (e) throw Error(e)
+        if (!e && r.statusCode === 200) {
             let arr = []
             let good = []
             let completed = []
             const $ = cheerio.load(b)
-            $('tbody tr').each((i, el)=>
-                {if(i<(len+start+1) && i>start)
+            $('tbody tr').each((i, el) => {
+                if (i < (len + start + 1) && i > start)
                     arr.push([$($(el).find('td')[0]).text(), $($(el).find('td')[1]).text()])
-                }
+            }
             )
-                arr.forEach((el, i)=>{
-                    try{
-                        request({'url':'http://yandex.ru',
+            arr.forEach((el, i) => {
+                try {
+                    request({
+                        'url': 'http://google.com',
                         'proxy': `http://${el[0]}:${el[1]}`,
-                        timeout}, (error, response, body)=> {
-                            completed.push(i)
-                            console.log(completed)
-                            if(body)
-                                console.log(body.length)
-                            if (body && body.length > size)
-                                good.push([[`${el[0]}`],[`${el[1]}`]])
-                            if(arr.length == completed.length){
-                                console.log(good)
-                                if(good.length>0)
-                                    res.render('index', {good})
-                                else
-                                    res.send('')
-                            }
-                        })
-                    }
-                    catch(e){
-                        console.log(e)
+                        timeout
+                    }, (error, response, body) => {
                         completed.push(i)
-                    }
-                })
+                        console.log(completed)
+                        if (body)
+                            console.log(body.length)
+                        if (body && body.length > 10000)
+                            good.push([[`${el[0]}`], [`${el[1]}`]])
+                        if (arr.length == completed.length) {
+                            console.log(good)
+                            if (good.length > 0)
+                                res.render('index', { good })
+                            else
+                                res.send('')
+                        }
+                    })
+                }
+                catch (e) {
+                    console.log(e)
+                    completed.push(i)
+                }
+            })
         }
-    
+
     })
 })
 
-app.listen(process.env.PORT || 5000, _=>console.log('started'))
+app.listen(process.env.PORT || 5000, _ => console.log('started'))
