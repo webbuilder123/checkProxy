ec64a7dd15ca3aec2eb77b8fb31a1b7d10db9a08
webbuilder123 <31453129+webbuilder123@users.noreply.github.com>
2018-01-29 02:48:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const request = require('request')
 const cheerio = require('cheerio')
 const app = require('express')()
 const url = require('url')
+
 app.set("view engine", "ejs")
+
 app.get('/', (req, res) => {
     const query = url.parse(req.url, true).query
     console.log(query)
@@ -10,6 +12,7 @@ app.get('/', (req, res) => {
     const start = parseInt(query.s) || 0
     const timeout = parseInt(query.t) || 2000
     const size = parseInt(query.sz) || 1000
+    
     request('https://www.proxy.org/', (e, r, b) => {
         if (e) throw e
         if (!e && r.statusCode === 200) {
@@ -17,11 +20,12 @@ app.get('/', (req, res) => {
             let good = []
             let completed = []
             const $ = cheerio.load(b)
+            
             $('tbody tr').each((i, el) => {
                 if (i < (len + start + 1) && i > start)
                     arr.push([$($(el).find('td')[0]).text(), $($(el).find('td')[1]).text()])
-            }
-            )
+            })
+            
             arr.forEach((el, i) => {
                 try {
                     request({
