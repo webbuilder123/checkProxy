2bf5b419a1523e3a7e9122f3f00e212b1498d43c
webbuilder123 <31453129+webbuilder123@users.noreply.github.com>
2018-01-29 15:09:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,30 +2,27 @@ const request = require('request')
 const cheerio = require('cheerio')
 const app = require('express')()
 const url = require('url')
-
+const uri = 'https://www.us-proxy.org/'
 app.set("view engine", "ejs")
-
 app.get('/', (req, res) => {
     const query = url.parse(req.url, true).query
     console.log(query)
-    const len = parseInt(query.l) || 30
+    const len = parseInt(query.l) || 15
     const start = parseInt(query.s) || 0
-    const timeout = parseInt(query.t) || 2000
-    const size = parseInt(query.sz) || 1000
-    
-    request('https://www.proxy.org/', (e, r, b) => {
+    const timeout = parseInt(query.t) || 1500
+    const size = parseInt(query.sz) || 10000
+    request(uri , (e, r, b) => {
         if (e) throw e
         if (!e && r.statusCode === 200) {
             let arr = []
             let good = []
             let completed = []
             const $ = cheerio.load(b)
-            
             $('tbody tr').each((i, el) => {
                 if (i < (len + start + 1) && i > start)
                     arr.push([$($(el).find('td')[0]).text(), $($(el).find('td')[1]).text()])
-            })
-            
+            }
+            )
             arr.forEach((el, i) => {
                 try {
                     request({
@@ -54,4 +51,26 @@ app.get('/', (req, res) => {
     })
 })
 
+app.get('/list', (req, res) => {
+    const query = url.parse(req.url, true).query
+    console.log(query)
+    const len = parseInt(query.l) || 30
+    const start = parseInt(query.s) || 0
+
+    request(uri , (e, r, b) => {
+        if (e) throw e
+        if (!e && r.statusCode === 200) {
+            let list = []
+            const $ = cheerio.load(b)
+            $('tbody tr').each((i, el) => {
+                if (i < (len + start + 1) && i > start)
+                    list.push([$($(el).find('td')[0]).text() +':'+ $($(el).find('td')[1]).text()])
+            })
+            res.render('list', {list})
+        }
+        else
+            res.send('none')
+
+    })
+})
 app.listen(process.env.PORT || 5000, _ => console.log('started'))
